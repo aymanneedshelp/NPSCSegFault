0d366b5604f7d402d28ef79162f6e5d810036f8a
Ayman Khan <ayman02khan@gmail.com>
2020-11-16 21:46:28 +0530

    changed sqlpass as a variable

--- Segfault.py
+++ Segfault.py
@@ -44,7 +44,7 @@ def numberlocations():#Numbering the locations
     f2.close()
     f3.close()
 def pushintosql():
-    mycon=mysql.connector.connect(host="localhost",user="root",passwd="sql123")
+    mycon=mysql.connector.connect(host="localhost",user="root",passwd=sqlpass)
     cursor=mycon.cursor()
     cursor.execute("Show databases")
     rec=cursor.fetchall()
@@ -54,14 +54,14 @@ def pushintosql():
             return
     cursor.execute("Create database if not exists covid")#DatabaseName:Covid
     mycon.close()
-    mycon=mysql.connector.connect(host="localhost",user="root",passwd="sql123",database="covid")
+    mycon=mysql.connector.connect(host="localhost",user="root",passwd=sqlpass,database="covid")
     cursor=mycon.cursor()
     cursor.execute("Create table if not exists Coviddataset(TimeofInfection int(4),Timeofreporting int(4),xlocation int(3),ylocation int(3),Age int(3),Diabetes varchar(10),Respiratoryillness varchar(10),AbnormalBloodPressure Varchar(10),Outcome varchar(10),Zone int(4))")
     cursor.execute("Create table if not exists population(Xlocation int(3),ylocation int(3),population int(10),zone int(5))")
     mycon.close()
     f1=open("COVID_Dataset Zone.csv",'r')
     csvr1=csv.reader(f1)
-    mycon=mysql.connector.connect(host="localhost",user="root",passwd="sql123",database="covid")
+    mycon=mysql.connector.connect(host="localhost",user="root",passwd=sqlpass,database="covid")
     cursor=mycon.cursor()
     for row in csvr1:
         if csvr1.line_num==1:
@@ -80,7 +80,7 @@ def pushintosql():
 
 
         
-    
+ sqlpass = input("Enter SQL Password ")   
 
 numberlocations()
 pushintosql()
