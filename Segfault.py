db263515fed88a619a41333b5c737841a8eaee89
karthikeyan456 <74282853+karthikeyan456@users.noreply.github.com>
2020-11-17 10:05:59 +0530

    Added a function to get zonewise data

--- Segfault.py
+++ Segfault.py
@@ -79,7 +79,173 @@ def pushintosql():
     mycon.close()
 
 
+def zonewise(zone,tableordata):
+        mycon=mysql.connector.connect(host="localhost",user="root",passwd="sql123",database="covid")
+        cursor=mycon.cursor()
+        cursor.execute("Select count(*) from coviddataset where zone=%s"%(zone))
+        rec=cursor.fetchall()
+        noofcases=0
+        for i in rec:
+           noofcases=i[0]
+        cursor.execute("Select count(*) from coviddataset where zone=%s and outcome='Dead'"%(zone))
+        rec=cursor.fetchall()
+        death=0
+        for i in rec:
+            death=i[0]
+        population=0
+        cursor.execute('Select population from population where zone=%s'%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            population=i[0]
+        try:
+                
+            deathrate=((death/noofcases)*100)
+        except:
+                deathrate=0
+        try:
+                
+            populationinfected=((noofcases/population)*100)
+        except:
+            populationinfected=0
+        age1=0
+        age2=0
+        age3=0
+        cursor.execute("Select count(*) from coviddataset where age<15 and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            age1=i[0]
+        cursor.execute("Select count(*) from coviddataset where age>=15 and age<=60 and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            age2=i[0]
+        cursor.execute("Select count(*) from coviddataset where age>60 and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            age3=i[0]
+        dbcount=0
+        bpcount=0
+        respicount=0
+        cursor.execute("Select count(*) from coviddataset where diabetes='TRUE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            dbcount=i[0]
+        cursor.execute("Select count(*) from coviddataset where Respiratoryillness='TRUE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+           respicount=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            bpcount=i[0]
+        com1=0
+        com2=0
+        com3=0
+        com4=0
+        com5=0
+        cursor.execute("Select count(*) from coviddataset where diabetes='TRUE' and Respiratoryillness='TRUE' and AbnormalBloodPressure='FALSE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            com1=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and Respiratoryillness='TRUE' and Diabetes='FALSE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            com2=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and diabetes='TRUE' and Respiratoryillness='FALSE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            com3=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and Respiratoryillness='TRUE'and diabetes='TRUE' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            com4=i[0]
+        com5=com1+com2+com3+com4
+        comdeath1=0
+        comdeath2=0
+        comdeath3=0
+        comdeath4=0
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath1=0
+        cursor.execute("Select count(*) from coviddataset where Respiratoryillness='TRUE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath2=0
+        cursor.execute("Select count(*) from coviddataset where diabetes='TRUE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath3=0
+        comdeath4=comdeath1+comdeath2+comdeath3
+        comdeath5=0
+        comdeath6=0
+        comdeath7=0
+        comdeath8=0
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and diabetes='TRUE' and respiratoryillness='FALSE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath5=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and Respiratoryillness='TRUE' and diabetes='FALSE'and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath6=i[0]
+        cursor.execute("Select count(*) from coviddataset where Diabetes='TRUE' and Respiratoryillness='TRUE' and AbnormalBloodPressure='FALSE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath7=i[0]
+        cursor.execute("Select count(*) from coviddataset where AbnormalBloodPressure='TRUE' and diabetes='TRUE'and Respiratoryillness='TRUE' and Outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            comdeath8=i[0]
+        comdeath9=comdeath5+comdeath6+comdeath7+comdeath8
+        aged1=0
+        aged2=0
+        aged3=0
+        cursor.execute("Select count(*) from coviddataset where age<15 and outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            aged1=i[0]
+        cursor.execute("Select count(*) from coviddataset where age>=15 and age<=60 and outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            aged2=i[0]
+        cursor.execute("Select count(*) from coviddataset where age>60 and outcome='Dead' and zone=%s"%(zone))
+        rec=cursor.fetchall()
+        for i in rec:
+            aged3=i[0]
+        populationinfectedp=round(populationinfected,2)
+        deathperc=round(deathrate,2)
+        comdeath10=death-(comdeath4+comdeath9)
         
+        txt1="General Information of zone "+str(zone)+"\n"
+        data1=[['Population',str(population)],['Number of cases',str(noofcases)],["Percentage of population infected",str(round(populationinfected,2))+"%"]]
+        headers=['Particulars','Count']
+        table1=columnar(data1,headers)
+        #print(txt1)
+        #print(table1)
+       
+        txt2="Age wise analysis of cases of zone "+str(zone)+"\n"
+        data2=[['Less than 15 years',str(age1)],['15 to 60 years',str(age2)],['Above 60 years',str(age3)]]
+        table2=columnar(data2,headers)
+        txt3="Analysis of cases of "+str(zone)+" based on comorbities"+'\n'
+        data3=[["Diabetes",str(dbcount)],["Respiratory disorders",str(respicount)],["Abnormal Blood Pressure",str(bpcount)],["Multiple comorbidities",str(com5)]]
+        table3=columnar(data3,headers)
+        txt4="Analysis of deaths of zone "+str(zone)+"\n"
+        data4=[["Number of deaths",str(death)],["Death rate",str(round(deathrate,2))+"%"],["Deaths with single comorbidities",str(comdeath4)],["Deaths with multiple comorbidities",str(comdeath9)],["Deaths without comorbidities",str(death-(comdeath4+comdeath9))],['Less than 15 years',str(aged1)],['Between 15 to 60 years',str(aged2)],['Above 60 years',str(aged3)]]
+        
+        table4=columnar(data4,headers)
+        msg=txt1+table1+txt2+table2+txt3+table3+txt4+table4
+        if tableordata==1:
+                print(msg)
+                return
+        if tableordata==0:
+                return population,noofcases,populationinfectedp,age1,age2,age3,dbcount,respicount,bpcount,com5,death,deathperc,comdeath4,comdeath9,comdeath10,aged1,aged2,aged3
+
+                
+        
+        
+
+   
+               
     
 
 numberlocations()
